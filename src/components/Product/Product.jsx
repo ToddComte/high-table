736f83a7752d8caf4ac21b9b70ca22693184343d
ToddComte <comtetodd@gmail.com>
2022-06-03 15:22:30 -0600

    Adding the add to cart button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -10,18 +10,26 @@ import {
 import { AddShoppingCart, CallMissedSharp } from "@material-ui/icons";
 import { mergeClasses } from "@material-ui/styles";
 
-const Product = () => {
+const Product = (product) => {
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image="" title={Product.name} />
+      <CardMedia className={classes.media} image="" title={product.name} />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography variant="h5" gutterBottom>
-            {Product.name}
-            <Typography variant="h5">{Product.price}</Typography>
+            {product.name}
+            <Typography variant="h5">{product.price}</Typography>
           </Typography>
         </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
       </CardContent>
+      <CardActions disableSpacing classes={classes.cardActions}>
+        <IconBotton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconBotton>
+      </CardActions>
     </Card>
   );
 };
